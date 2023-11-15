77b051822efb0aa522438a1fb6fbd30e41c77155
Michael Verhoeven <michael.verhoeven.mrv@gmail.com>
2023-11-15 13:44:04 -0600

    updates surveys with newest aquatic plant adds, adds Katie's phytoplankton data and script to read and reformat

--- Scripts/ramsey_county_phytoplankton_dataset_assembly.R
+++ Scripts/ramsey_county_phytoplankton_dataset_assembly.R
@@ -1,6 +1,6 @@
-#'---
-#' title: "Plant Data Assembly"
-#' author: "Mike Verhoeven"
+'---
+#' title: "Phyto_data_cleaning"
+#' author: "Katie Polik"
 #' output: 
 #'    html_document:
 #'       toc: true
@@ -10,7 +10,7 @@
 #'           collapsed: false
 #'---
 
-#' This script will filter the full MN plants survey db for ramsey county lakes, then add in a bunch of other surveys that have been added since that project ended. 
+#' This script takes an aggregated Ramsey county phytoplankton clean(ish) csv with all the cell count concentration data and rearranges and visualizes it. There are still a few data issues that we may need to check with John Manske about, but I'm pretty sure I've caught and corrected the vast majority of the typos at this point
 #' 
 
 
@@ -19,7 +19,113 @@
 
 # load libraries ------------------------------------------------------------------
 # # Load Libraries
-library(data.table)
-# update_dev_pkg()# remotes::install_github("Rdatatable/data.table")
+
+
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+
 
 # load in data -------------------------------------------------
+# # 
+CellCounts <- read.csv("data/Ramsey_Phyto_CellCounts.csv")
+PhytoID <- read.csv("PhytoID.csv")
+
+## Data Wrangling  -------------------------
+# Transform data into long format
+sp_vec <- select(CellCounts, starts_with("NAME")) %>% as.matrix() %>% t() %>% as.vector()
+co_vec <- select(CellCounts, starts_with("CONC")) %>% as.matrix() %>% t() %>% as.vector()
+col_num <- length(names(select(CellCounts, starts_with("NAME"))))
+
+CellCounts.long <- tibble(DNRID = rep(unlist(CellCounts[,"DNRID"]), each = col_num),
+                          DATE = rep(unlist(CellCounts[,"DATE"]), each = col_num),
+                          SITE = rep(unlist(CellCounts[,"SITE"]), each = col_num),
+                          NAME = sp_vec,
+                          CONC.NO.ML = co_vec) %>% 
+                    subset(NAME != "#N/A") %>%
+                    mutate(CONC.NO.ML = as.numeric(CONC.NO.ML))
+
+# Transform data into wide format
+CellCounts.wide <- CellCounts.long %>%
+  pivot_wider(id_cols = c(DNRID, DATE, SITE), 
+              names_from = NAME, 
+              values_from = CONC.NO.ML, 
+              values_fn = sum)
+
+#NOTE Sometimes a single taxa will have multiple count entries for one site/date.
+#You can find the duplicates by going into CellCounts.wide and searching for ","
+#for now we solve this by summing, making the assumption that these counts are split for some reason?
+
+## Group Counts --------------------------
+# I have checked that all of the taxa (92) in CellCounts.wide are also in PhytoID. 
+# If more taxa get added to CellCounts.wide, you should check again.
+# (e.g. when we add the 2023 data)
+
+# subset the phytoIDs to only be the taxa present in the cell counts
+TAXA.counts <- names(CellCounts.wide)
+TAXA.ids <- as.vector(PhytoID$NAME)
+unused.taxa <- setdiff(TAXA.ids, TAXA.counts)
+PhytoID.sub <- PhytoID[! PhytoID$NAME %in% unused.taxa,2:3]
+PhytoID.sub <- PhytoID.sub[!duplicated(PhytoID.sub),] #some taxa have two IDs??
+                
+# make vectors of taxa names within each group
+cyanophyta <- as.vector(subset(PhytoID.sub, GROUP == "CYANOPHYTA (BLUE-GREEN)")$NAME)
+chlorophyta <- as.vector(subset(PhytoID.sub, GROUP == "CHLOROPHYTA (GREEN)")$NAME)
+diatom <- as.vector(subset(PhytoID.sub, GROUP == "BACILLARIOPHYCEAE (DIATOM)")$NAME)
+euglenophyta <- as.vector(subset(PhytoID.sub, GROUP == "EUGLENOPHYTA (EUGLENOIDS)")$NAME)
+dinoflag <- as.vector(subset(PhytoID.sub, GROUP == "PYRRHOPHYTA (DINOFLAGELLATES)")$NAME)
+cryptophyta <- as.vector(subset(PhytoID.sub, GROUP == "CRYPTOPHYTA (CRYPTOMONADS)")$NAME)
+chrysophyta <- as.vector(subset(PhytoID.sub, GROUP == "CHRYSOPHYTA (YELLOW-GREEN)")$NAME)
+taxa.groups <- c("CYANOS", "GREEN", "DIATOM" , "EUGLENA", "DINOS", "CRYPTOS", "CHRYSOS")
+
+# Calculate Group Counts
+CellCounts.groups <- CellCounts.wide %>% 
+  mutate(CYANOS = rowSums(.[cyanophyta], na.rm = T),
+         GREEN = rowSums(.[chlorophyta], na.rm = T),
+         DIATOM = rowSums(.[diatom], na.rm = T),
+         EUGLENA = rowSums(.[euglenophyta], na.rm = T),
+         DINOS = rowSums(.[dinoflag], na.rm = T),
+         CRYPTOS = rowSums(.[cryptophyta], na.rm = T),
+         CHRYSOS = rowSums(.[chrysophyta], na.rm = T)) %>%
+  mutate(ALL.CELLS = rowSums(.[taxa.groups], na.rm = T)) %>%
+  mutate(CYANOS.rel = CYANOS/ALL.CELLS, 
+         GREEN.rel = GREEN/ALL.CELLS, 
+         DIATOM.rel = DIATOM/ALL.CELLS, 
+         EUGLENA.rel = EUGLENA/ALL.CELLS, 
+         DINOS.rel = DINOS/ALL.CELLS, 
+         CRYPTOS.rel = CRYPTOS/ALL.CELLS, 
+         CHRYSOS.rel = CHRYSOS/ALL.CELLS )
+
+# here is where we could also add calculations of diversity  
+# simplify data into smaller dataframe if we only care about groups
+GroupCounts <- CellCounts.groups %>%
+  select(DNRID, DATE, SITE, ALL.CELLS, all_of(taxa.groups), 
+         CYANOS.rel, GREEN.rel, DIATOM.rel, EUGLENA.rel, DINOS.rel, CRYPTOS.rel, CHRYSOS.rel)
+  
+## Data playtime! ----------------------------------
+
+#Note that cell counts aren't always a great way to think about data
+#Most people prefer biovolume (I think)
+#We should decided if we absolutely need biovolume or if we can get away with just cell counts.
+
+ISLAND <- GroupCounts %>%
+  subset(DNRID == "62-0075" & DATE <= "20120000" & DATE >= "20100000") %>%
+  select(DNRID, DATE, SITE, all_of(taxa.groups))
+
+ISLAND.long <- ISLAND %>%
+  pivot_longer(cols = all_of(taxa.groups), names_to = "group") %>%
+  mutate(DATE = ymd(DATE)) %>%
+  mutate(YEAR = year(DATE),
+         month_day = as_date(paste(2023,month(DATE),day(DATE), sep = "-")))
+
+ggplot(ISLAND.long, aes(fill=group, y=value, x=DATE)) + 
+  geom_bar(position="fill", stat="identity") #+
+  #facet_wrap(~YEAR)
+
+#my takeaway from playing with data briefly is that stacked bar-chart timeseries are slightly annoying to plot because of all the winter time gaps
+#I sort of tried to do this with facets but didn't try very hard (and it didn't work)
+           
+  
+
+
+
--- Scripts/ramsey_county_plant_dataset_assembly.R
+++ Scripts/ramsey_county_plant_dataset_assembly.R
@@ -21,6 +21,9 @@
 # # Load Libraries
 library(data.table)
 # update_dev_pkg()# remotes::install_github("Rdatatable/data.table")
+library(janitor)
+
+
 
 # load in data -------------------------------------------------
 # # 
@@ -28,7 +31,110 @@ library(data.table)
 # plants_occurrence_wide <- fread(file = "data&scripts/data/output/plants_env_data_wide.csv", drop = 1)
 # plants_rakeabund_wide <- fread(file = "data&scripts/data/output/plants_abund_env_data_wide.csv")
 surveys <- fread(file = "E:\\My Drive\\Documents\\UMN\\Grad School\\Larkin Lab\\R_projects\\MN_aquatic_plants_synthesis\\data&scripts\\data\\output\\surveys_aqplants.csv")
+
+#unprocessed plant data
 plants_db_new <- readRDS("E:/My Drive/Documents/UMN/Grad School/Larkin Lab/R_projects/MN_aquatic_plants_synthesis/data&scripts/data/input/plants_db_new.rds")
+
+plants_db_new <- clean_names(plants_db_new)
+
+cols <- c("submitter_name", "submitter_email", "submit_time",
+          "dow", "survey_start", "surveyors", 
+          "rake_max", 
+          "sta_nbr", "x", "sta_nbr_1", "sample_type", "guid", "latitude", "longitude", 
+          "depth", "depth_units", "substrate",  "depth_1", 
+          "whole_rake_density", "no_veg_found"
+          
+          )
+
+setcolorder(plants_db_new, cols)
+
+plants_db_new[ , .N , .(is.na(depth), is.na(depth_1), no_veg_found) ]
+
+
+# collapse new surveys to match the surveys format: -----------------------
+
+#drop points without depth data
+plants_db_new <- plants_db_new[ !is.na(depth)|!is.na(depth_1) , , ]
+
+# depths?
+    plot(depth~depth_1, data = plants_db_new[ !is.na(depth) & !is.na(depth_1), .(depth, depth_1) ,])
+    
+    plants_db_new[ (!is.na(depth) & !is.na(depth_1)) & depth_1>depth, .(depth, depth_1) ,]
+    
+    #meters in depth
+    plot(depth~depth_1, data = plants_db_new[ (!is.na(depth) & !is.na(depth_1)) & (as.numeric(depth_1)/as.numeric(depth))< 1, .(depth, depth_1) ,])
+    #feet in depth
+    plot(depth~depth_1, data = plants_db_new[ (!is.na(depth) & !is.na(depth_1)) & (as.numeric(depth_1)/as.numeric(depth))> 1, .(depth, depth_1) ,])
+    
+    #make explicit column
+    plants_db_new[ (!is.na(depth) & !is.na(depth_1)) & (as.numeric(depth_1)/as.numeric(depth))> 1, depth_feet := depth_1 ,]
+    plants_db_new[ (!is.na(depth) & !is.na(depth_1)) & (as.numeric(depth_1)/as.numeric(depth))< 1, depth_feet := depth_1*3.28084 ,]
+    plants_db_new[  , .N , depth_units      ]
+    plants_db_new[ depth_units == "Meters" , depth_feet := as.numeric(depth) * 3.28084 , ]
+    plants_db_new[ depth_units == "Feet" , depth_feet := as.numeric(depth) , ]
+    plants_db_new[ , summary(depth_feet) ,   ]
+    
+    plants_db_new[ is.na(depth_feet), .N , .(depth, depth_1)] #revisit these to fix them up [someday! not now]
+    
+# survey ident
+    plants_db_new[ , .N ,  .(dow, survey_start)]
+    plants_db_new[ , survey_ident := .GRP ,  .(dow, survey_start)]    
+    
+#point idents
+    #given valus:
+    plants_db_new[ , .N , .(is.na(sta_nbr), is.na(sta_nbr_1), is.na(x), is.na(latitude), is.na(longitude)) ]
+    #lets build our own
+    plants_db_new[ , point_ident := seq_len(.N)  , .(survey_ident) ]
+    
+    
+#organize:
+    cols <- c("submitter_name", "submitter_email", "submit_time",
+              "dow", "survey_start", "survey_ident",
+              "rake_max", "surveyors", "sta_nbr", "x", "sta_nbr_1", "sample_type", "guid", "latitude", "longitude", "point_ident", "x_2", "y",
+              "depth", "depth_units", "substrate",  "depth_1", "depth_feet", 
+              "whole_rake_density", "no_veg_found", "plant_height", "plant_height_1", "percent_biovolume",
+              "please_provide_comment"
+              
+    )
+              
+    
+    setcolorder(plants_db_new, cols)
+    taxalist <- names(plants_db_new)[!names(plants_db_new)%in%cols]
+    
+#melt long:
+    plants_new_long <- melt(plants_db_new, id.vars = cols , variable.name = c( "taxon"))
+    plants_new_long[ , .N , value]
+    plants_new_long <- plants_new_long[!is.na(value)]
+    
+    #looks like some depths or other zany things made it in here:
+    plants_new_long[ , sort(unique(value)) , ]
+    
+    #what column were containing shitty stuff
+    plants_new_long[!value%in%c("X", "2", "4", "3", "1", "x", "0", "5"), .N , taxon]
+    plants_new_long[  , .N  , rake_max ] #hows that for a  revelation!!!
+    
+    #zeros are non-occurrences, drop those
+    plants_new_long[ value == "0" , .N , submitter_name ]
+    plants_new_long <- plants_new_long[value != "0"]
+    plants_new_long[ , taxa_present := is.na(value) , ]
+    plants_new_long[rake_max%in%c("3","4","5") , taxa_rakeabund := value , ]
+    
+    plants_new_long[ , .N , taxa_rakeabund]
+    
+    plants_new_long[value%in%c("x","X"), .N , survey_ident ]
+    
+    
+# survey metrics
+    
+    plants_db_new[ , .N ,  .(dow, survey_start, survey_ident)]
+    plants_db_new[ , survey_ident := .GRP ,  .(dow, survey_start)]
+    
+    surveys_new <- plants_db_new[ , .(nobs = .N, 
+                                      tot_n_samp) ,  .(dow, survey_start)]
+
+
+
+
 
 
 
@@ -52,7 +158,7 @@ ramsey_co_plant_surveys[ ,.N ,]
 ramsey_co_plant_surveys[ , .N , SURVEY_ID] 
 ramsey_co_plant_surveys[SUBBASIN != "" ,  , ]
 
-ramsey_co_plant_surveys[ , .(SURVEY_ID, DOW, SUBBASIN, fw_id, DATASOURCE, LAKE_NAME, DATESURVEYSTART, MULTIPARTSURVEY, nobs, tot_n_samp, ), ]
+ramsey_co_plant_surveys[ , .N, .(SURVEY_ID, DOW, SUBBASIN, fw_id, SURVEY_DATASOURCE, LAKE_NAME, DATESURVEYSTART, MULTIPARTSURVEY, nobs, tot_n_samp ) ]
 
 
 #merge to invasion status
